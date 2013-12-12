f7f28ab1622163c862a143a3e4518029e76a1a44
flashphoner <support@flashphoner.com>
2013-12-12 22:08:00 +0600

    [MERGE] cef0e2915d19601559493c20b936567872c9c6ee
    * [ADD] FPNR-409, Add wss port to load balancer response.

--- client/client/src/js/FlashphonerLoader.js
+++ client/client/src/js/FlashphonerLoader.js
@@ -183,7 +183,7 @@ FlashphonerLoader.prototype = {
                 isWebRTCAvailable = false;
             } else {
                 console.log("Bad streaming property " + streamingType.text() +
-                    ", can be wertc or flash. Using default behaviour!")
+                    ", can be webrtc or flash. Using default behaviour!")
             }
         }
 
@@ -212,11 +212,13 @@ FlashphonerLoader.prototype = {
                 success: function (loadBalancerData) {
                     me.wcsIP = loadBalancerData.server;
                     me.wsPort = loadBalancerData.ws;
+                    me.wssPort = loadBalancerData.wss;
                     me.flashPort = loadBalancerData.flash;
                     me.jsonpSuccess = true;
                     trace("Connection data from load balancer: "
                         + "wcsIP " + loadBalancerData.server
                         + ", wsPort " + loadBalancerData.ws
+                        + ", wssPort " + loadBalancerData.wss
                         + ", flashPort " + loadBalancerData.flash);
                     me.loadAPI();
                 }
