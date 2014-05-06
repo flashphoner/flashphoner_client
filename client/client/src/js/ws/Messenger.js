d4e85fa0d16f4a49bca933f75225628bfc972c5e
Flashphoner <support@flashphoner.com>
2014-05-06 14:02:35 +1000

    [FIX] notifyDeliveryFailed parameters in Messenger.notifyMessage.

--- client/client/src/js/ws/Messenger.js
+++ client/client/src/js/ws/Messenger.js
@@ -26,7 +26,7 @@ Messenger.prototype = {
         } else if (message.state == "IMDN_DELIVERED") {
             this.notifyDelivered(message, notificationResult);
         } else if (message.state == "IMDN_FAILED" || message.state == "IMDN_FORBIDDEN" || message.state == "IMDN_ERROR") {
-            this.notifyDeliveryFailed(message.notificationResult);
+            this.notifyDeliveryFailed(message, notificationResult);
         } else if (message.state == "RECEIVED") {
             //here we will choose what to display on multiple contacts in "from".
             if (message.from.indexOf(",") != -1) {
