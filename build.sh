4adce0a5896181b6a18f23abc3f93162751db92a
flashphoner <support@flashphoner.com>
2014-11-14 17:11:42 +0600

    [ADD] Build script

--- build.sh
+++ build.sh
@@ -1,7 +1,24 @@
-﻿#!/bin/sh
-FULL_PATH=$0
-CURRENT_DIR=`dirname $FULL_PATH`
-cd $CURRENT_DIR
-cd builder
-chmod +x builder.sh
-./builder.sh
+#!/bin/sh
+BASE_DIR=`pwd`
+. ./builder/functions.lib
+
+echo "" > $BASE_DIR"/log"
+
+echo "Start clients build..."
+build_client $BASE_DIR
+echo "Build clients complete"
+echo ""
+    
+echo "Creating archive of the build"
+cd $BASE_DIR
+rm -rf output
+mkdir output
+cp client/src/examples output -R
+cp client/src/Flashphoner.js output
+mkdir output/dependencies
+cp client/src/dependencies/jquery output/dependencies -R
+cp client/src/dependencies/swf output/dependencies -R
+mkdir output/dependencies/flash
+cp client/src/dependencies/flash/asdoc output/dependencies/flash -R
+cp client/src/dependencies/flash/MediaManager.swf output/dependencies/flash
+echo "FINISH"
