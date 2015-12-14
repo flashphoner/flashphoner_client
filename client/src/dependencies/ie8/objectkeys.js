c3fb94dba2b2429d95f786beb5a3d75fcdf52d36
Flashphoner <support@flashphoner.com>
2015-12-14 12:33:59 +0400

    [ADD] IE8 Object.keys support

---
+++ client/src/dependencies/ie8/objectkeys.js
@@ -0,0 +1,10 @@
+// Add compatible Object.keys support in older environments
+
+
+Object.keys = function(o) {
+    if (o !== Object(o))
+        throw new TypeError('Object.keys called on a non-object');
+    var k=[],p;
+    for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) k.push(p);
+    return k;
+}
