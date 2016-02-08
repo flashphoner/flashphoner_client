7bbe1a7444bf66f7211691139340f4e78e4bbd4d
Flashphoner <support@flashphoner.com>
2016-02-08 17:49:00 +0600

    [Fix] WS Player migrated to the new API with mediaProvider WSPlayer

--- client/src/examples/vow-player/js/vow-player.js
+++ client/src/examples/vow-player/js/vow-player.js
@@ -8,7 +8,7 @@ config.videoWidth = 320;
 config.videoHeight = 240;
 
 //Web Call Server Websocket URL
-var url = "ws://192.168.1.5:8080";
+var url = setURL();
 
 //The stream name can be an RTSP URL for playback
 //Example: rtsp://192.168.1.5:1935/live/stream1
@@ -20,42 +20,34 @@ var streamName;
 //Get API instance
 var f = Flashphoner.getInstance();
 
-//get player instance
-var wsPlayer;
-
 //Current stream
 var stream = {};
+
+///////////////////////////////////////////
+//////////////Init////////////////////
+
+$(document).ready(function () {
+    $("#controlButton").addClass("playButton");
+    $(".control").click(function () {
+        if (stream.status != undefined && stream.status != StreamStatus.Stoped) {
+            return;
+        }
+        playStream();
+    });
+});
 
 function initOnLoad() {
     //add listeners
     f.addListener(WCSEvent.ErrorStatusEvent, errorEvent);
     f.addListener(WCSEvent.ConnectionStatusEvent, connectionStatusListener);
     f.addListener(WCSEvent.StreamStatusEvent, streamStatusListener);
-    f.addListener(WCSEvent.OnBinaryEvent, binaryListener);
-    f.init();
-
-    //create player
-    var canvas = document.getElementById('videoCanvas');
-    wsPlayer = new WebsocketPlayer(canvas, function(e){
-            if (e.unmute != undefined) {
-                if (this.stream.status == StreamStatus.Paused) {
-                    f.playStream(this.stream);
-                    console.log("Request stream back");
-                    onPlaying();
-                }
-            } else if (e.mute != undefined) {
-                if (this.stream.status == StreamStatus.Playing) {
-                    f.pauseStream(this.stream);
-                    console.log("pauseStream");
-                    onPaused();
-                }
-            }
-        }.bind(this),
-        function (str) {
-            this.writeInfo(str);
-        }.bind(this)
-    );
-    wsPlayer.init(config);
+    var configuration = new Configuration();
+    configuration.wsPlayerCanvas = document.getElementById('videoCanvas');
+    configuration.wsPlayerReceiverPath = "../../dependencies/websocket-player/WSReceiver.js";
+    configuration.videoWidth = 320;
+    configuration.videoHeight = 240;
+    f.init(configuration);
+    initVisibility();
 
     //connect to server
     f.connect({
@@ -76,27 +68,19 @@ function connectionStatusListener(event) {
         console.log('Connection has been established. Press Play to get stream.');
         writeInfo("CONNECTED, press play");
     } else if (event.status == ConnectionStatus.Disconnected) {
-        wsPlayer.stop();
         console.log("Disconnected");
         writeInfo("DISCONNECTED");
     } else if (event.status == ConnectionStatus.Failed) {
-        wsPlayer.stop();
         writeInfo("CONNECTION FAILED");
         f.disconnect();
     }
-}
-
-function binaryListener(event) {
-    wsPlayer.onDataReceived(event);
 }
 
 //Connection Status
 function streamStatusListener(event) {
     console.log(event.status);
     if (event.status == StreamStatus.Failed) {
-        wsPlayer.stop();
     } else if (event.status == StreamStatus.Stoped) {
-        wsPlayer.stop();
     } else if (event.status == StreamStatus.Playing){
         onPlaying();
     } else if (event.status == StreamStatus.Paused){
@@ -109,12 +93,15 @@ function streamStatusListener(event) {
 //Error
 function errorEvent(event) {
     console.log(event.info);
-    wsPlayer.stop();
 }
-//////////////////////////////////////////
+
+
+///////////////////////////////////////////
+//////////////Controls////////////////////
+
 
 function playFirstSound() {
-    wsPlayer.playFirstSound();
+    f.playFirstSound();
 }
 
 function playStream() {
@@ -123,18 +110,7 @@ function playStream() {
     var stream = new Stream();
     stream.name = document.getElementById("streamId").value;
     stream.hasVideo = true;
-    stream.mediaProvider = MediaProvider.Flash;
-    stream.sdp = "v=0\r\n" +
-    "o=- 1988962254 1988962254 IN IP4 0.0.0.0\r\n" +
-    "c=IN IP4 0.0.0.0\r\n" +
-    "t=0 0\r\n" +
-    "a=sdplang:en\r\n"+
-    "m=video 0 RTP/AVP 32\r\n" +
-    "a=rtpmap:32 MPV/90000\r\n" +
-    "a=recvonly\r\n" +
-    "m=audio 0 RTP/AVP 0\r\n" +
-    "a=rtpmap:0 PCMU/8000\r\n" +
-    "a=recvonly\r\n";
+    stream.mediaProvider = MediaProvider.WSPlayer;
     this.stream = f.playStream(stream);
 }
 
@@ -143,28 +119,84 @@ function writeInfo(str) {
     div.innerHTML = div.innerHTML + str + "<BR>";
 }
 
+///////////////////////////////////////////
+//////////////Display UI////////////////////
+
 function onPlaying(){
     $("#controlButton").removeClass("playButton");
     $("#controlButton").addClass("pauseButton");
-    $(".control").click(function () {
-        wsPlayer.pause();
+    $("#controlButton").unbind();
+    $("#controlButton").click(function () {
+        f.pauseStream(stream);
     });
 }
 
 function onPaused(){
     $("#controlButton").removeClass("pauseButton");
     $("#controlButton").addClass("playButton");
-    $(".control").click(function () {
-        wsPlayer.resume();
+    $("#controlButton").unbind();
+    $("#controlButton").click(function () {
+        f.playStream(stream);
     });
 }
 
-$(document).ready(function () {
-    $("#controlButton").addClass("playButton");
-    $(".control").click(function () {
-        if (stream.status != undefined && stream.status != StreamStatus.Stoped) {
-            return;
+/////////////////////////////////////////////////////
+///////////////Page visibility///////////////////////
+
+var hidden = undefined;
+function initVisibility() {
+    var visibilityChange;
+    if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+        this.hidden = "hidden";
+        visibilityChange = "visibilitychange";
+    } else if (typeof document.mozHidden !== "undefined") {
+        this.hidden = "mozHidden";
+        visibilityChange = "mozvisibilitychange";
+    } else if (typeof document.msHidden !== "undefined") {
+        this.hidden = "msHidden";
+        visibilityChange = "msvisibilitychange";
+    } else if (typeof document.webkitHidden !== "undefined") {
+        this.hidden = "webkitHidden";
+        visibilityChange = "webkitvisibilitychange";
+    }
+    if (typeof this.hidden === "undefined") {
+        console.error("Visibility API not supported, player will continue to play when in background");
+    } else {
+        document.addEventListener(visibilityChange, visibilityHandler.bind(this), false);
+    }
+}
+
+function visibilityHandler() {
+    if (document[this.hidden]) {
+        console.log("Document hidden, mute player");
+        if (stream && stream.status == StreamStatus.Playing) {
+            f.mute(MediaProvider.WSPlayer);
         }
-        playStream();
-    });
-});
+    } else {
+        console.log("Document active, unmute player");
+        if (stream && stream.status == StreamStatus.Playing) {
+            f.unmute(MediaProvider.WSPlayer);
+        }
+    }
+}
+
+
+/////////////////////////////////////////////////////
+///////////////Utils/////////////////////////////////
+
+//set WCS URL
+function setURL() {
+    var proto;
+    var url;
+    var port;
+    if (window.location.protocol == "http:") {
+        proto = "ws://";
+        port = "8080";
+    } else {
+        proto = "wss://";
+        port = "8443";
+    }
+
+    url = proto + window.location.hostname + ":" + port;
+    return url;
+}
