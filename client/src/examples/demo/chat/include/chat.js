05ae1838347e3ae56e03bbc7a319635dcbb50ebb
Flashphoner <support@flashphoner.com>
2016-03-04 10:45:20 +0400

    [FIX] some chat fixes

--- client/src/examples/demo/chat/include/chat.js
+++ client/src/examples/demo/chat/include/chat.js
@@ -93,7 +93,8 @@ function dataEventListener(event) {
     var message = event.payload;
     var newMessage = time + " " + message.from + " - " + message.body + '<br/>';
     var chat = document.getElementById("chat");
-    chat.innerHTML = newMessage + chat.innerHTML;
+    chat.innerHTML += newMessage;
+    chat.scrollTo(0,chat.scrollHeight);
 }
 
 // Set connection status and display corresponding view
