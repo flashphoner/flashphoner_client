723f956e5b033d46a0a6cdc782fb6e71c32de25e
Flashphoner <support@flashphoner.com>
2016-09-23 11:33:14 +1000

    [REFACTOR] recording.js example

--- examples/demo/streaming/stream_recording/recording.js
+++ examples/demo/streaming/stream_recording/recording.js
@@ -1,179 +1,134 @@
-//Init API
-Flashphoner.init({flashMediaProviderSwfLocation: '../../../../media-provider.swf'});
-
 var SESSION_STATUS = Flashphoner.constants.SESSION_STATUS;
 var STREAM_STATUS = Flashphoner.constants.STREAM_STATUS;
-var currentSession;
-var _stream;
-var _streamName;
-var _fileName;
 var localVideo;
 
-
-//////////////////////////////////
-/////////////// Init /////////////
-
 function init_page() {
-    $("#url").val(setURL() + "/" + createUUID(8));
-    $("#downloadDiv").hide();
-    $("#publishBtn").click(function () {
-            $(this).prop('disabled',true);
-            var state = $("#publishBtn").text();
-            if (state == "Start") {
-                connectAndPublish();
-            } else {
-                unPublishStream();
-            }
-        }
-    );
-}
-
-function initAPI() {
+    //init api
+    try {
+        Flashphoner.init({flashMediaProviderSwfLocation: '../../../../media-provider.swf'});
+    } catch(e) {
+        $("#notifyFlash").text("Your browser doesn't support Flash or WebRTC technology necessary for work of an example");
+        return;
+    }
 
     localVideo = document.getElementById("localVideo");
 
-    if (detectIE()) {
-        detectFlash();
-    }
-
-    init_page();
+    $("#url").val(setURL() + "/" + createUUID(8));
+    $("#downloadDiv").hide();
+    onStopped();
 }
 
-///////////////////////////////////
-///////////// Controls ////////////
-///////////////////////////////////
+function onStarted(stream) {
+    $("#publishBtn").text("Stop").off('click').click(function(){
+        $(this).prop('disabled', true);
+        stream.stop();
+    }).prop('disabled', false);
+}
 
-//New connection
-function connectAndPublish() {
-    if (currentSession && currentSession.status() == SESSION_STATUS.ESTABLISHED) {
-        console.warn("Already connected, session id " + currentSession.id());
-        publishStream();
-        return;
+function onStopped() {
+    $("#publishBtn").text("Start").off('click').click(function(){
+        if (validateForm()) {
+            $(this).prop('disabled', true);
+            $('#url').prop('disabled', true);
+            $("#downloadDiv").hide();
+            start();
+        }
+    }).prop('disabled', false);
+    $('#url').prop('disabled', false);
+}
+
+function start() {
+    var url = $('#url').val();
+    //check if we already have session
+    if (Flashphoner.getSessions().length > 0) {
+        var session = Flashphoner.getSessions()[0];
+        if (session.getServerUrl() == url) {
+            publishStream(session);
+            return;
+        } else {
+            //remove session DISCONNECTED and FAILED callbacks
+            session.on(SESSION_STATUS.DISCONNECTED, function(){});
+            session.on(SESSION_STATUS.FAILED, function(){});
+            session.disconnect();
+        }
     }
-    var url = field('url');
+    //create session
     console.log("Create new session with url " + url);
-    var handleSession = function (session) {
-        var status = session.status();
-        switch (status) {
-            case SESSION_STATUS.FAILED:
-                console.warn("Session failed, id " + session.id());
-                removeSession(session);
-                setStatus(status);
-                break;
-            case SESSION_STATUS.DISCONNECTED:
-                console.log("Session diconnected, id " + session.id());
-                removeSession(session);
-                setStatus(status);
-                break;
-            case SESSION_STATUS.ESTABLISHED:
-                console.log("Session established, id " + session.id());
-                publishStream();
-                break;
-        }
-    };
-    currentSession = Flashphoner.createSession({urlServer: url})
-        .on(SESSION_STATUS.FAILED, handleSession)
-        .on(SESSION_STATUS.DISCONNECTED, handleSession)
-        .on(SESSION_STATUS.ESTABLISHED, handleSession);
-}
-
-//Disconnect
-function disconnect() {
-    if (!currentSession) {
-        console.warn("Nothing to disconnect");
-        return;
-    }
-    currentSession.disconnect();
-    $("#connectBtn").text("Connect");
-}
-
-//Publish stream
-function publishStream() {
-    _streamName = field("url").split('/')[3];
-    if (!currentSession || currentSession.status() != SESSION_STATUS.ESTABLISHED) {
-        console.warn("Session is not ready or null");
-        return;
-    }
-
-    var handleStream = function(stream) {
-        var status = stream.status();
-        console.log("Stream status: " + status);
-        switch (status) {
-            case STREAM_STATUS.PUBLISHING:
-                _stream = stream;
-                _fileName = stream.getRecordInfo();
-                console.log("File name " + _fileName);
-            case STREAM_STATUS.FAILED:
-            case STREAM_STATUS.UNPUBLISHED:
-                setStatus(status);
-                break;
-
-        }
-    };
-
-    currentSession.createStream({name: _streamName, record: true, mediaProvider: Flashphoner.getMediaProviders()[0], display: localVideo, cacheLocalResources: true})
-        .on(STREAM_STATUS.PUBLISHING, handleStream)
-        .on(STREAM_STATUS.FAILED, handleStream)
-        .on(STREAM_STATUS.UNPUBLISHED, handleStream).publish();
+    Flashphoner.createSession({urlServer: url}).on(SESSION_STATUS.ESTABLISHED, function(session){
+        setStatus(session.status());
+        //session connected, start playback
+        publishStream(session);
+    }).on(SESSION_STATUS.DISCONNECTED, function(){
+        setStatus(SESSION_STATUS.DISCONNECTED);
+        onStopped();
+    }).on(SESSION_STATUS.FAILED, function(){
+        setStatus(SESSION_STATUS.FAILED);
+        onStopped();
+    });
 
 }
 
-//Stop stream publishing
-function unPublishStream() {
-    _stream.stop();
+function publishStream(session) {
+    var streamName = $('#url').val().split('/')[3];
+    session.createStream({
+        name: streamName,
+        display: localVideo,
+        record: true
+    }).on(STREAM_STATUS.PUBLISHING, function(stream) {
+        setStatus(stream.status());
+        onStarted(stream);
+    }).on(STREAM_STATUS.UNPUBLISHED, function(stream) {
+        setStatus(stream.status());
+        showDownloadLink(stream.getRecordInfo());
+        onStopped();
+    }).on(STREAM_STATUS.FAILED, function(stream) {
+        setStatus(stream.status());
+        showDownloadLink(stream.getRecordInfo());
+        onStopped();
+    }).publish();
 }
 
-/////////////////////////////////////
-///////////// Display UI ////////////
-/////////////////////////////////////
-
-// Set Connection Status
+//show connection or local stream status
 function setStatus(status) {
+    var statusField = $("#status");
+    statusField.text(status).removeClass();
     if (status == "PUBLISHING") {
-        $("#status").text(status).removeClass().attr("class","text-success");
-        $("#publishBtn").text("Stop");
-    }
-
-    if (status == "DISCONNECTED" || status == "UNPUBLISHED") {
-        $("#status").text(status).removeClass().attr("class","text-muted");
-        $("#publishBtn").text("Start");
-        if (_fileName) {
-            showDownloadLink(_fileName);
-        }
-    }
-
-    if (status == "FAILED") {
-        $("#status").text(status).removeClass().attr("class","text-danger");
-        $("#publishBtn").text("Start");
-    }
-    $("#publishBtn").prop("disabled",false);
-}
-
-// Check field for empty string
-function checkForEmptyField(checkField, alertDiv) {
-    if (!$(checkField).val()) {
-        $(alertDiv).addClass("has-error");
-        return false;
-    } else {
-        $(alertDiv).removeClass("has-error");
-        return true;
-    }
-}
-
-function removeSession(session) {
-    if (currentSession.id() == session.id()) {
-        currentSession = null;
+        statusField.attr("class","text-success");
+    } else if (status == "DISCONNECTED" || status == "ESTABLISHED" || status == "UNPUBLISHED") {
+        statusField.attr("class","text-muted");
+    } else if (status == "FAILED") {
+        statusField.attr("class","text-danger");
     }
 }
 
 // Show link to download recorded stream
-
 function showDownloadLink(name) {
-    // Set correct path for records. Stream records are saved to WCS_HOME/records directory.
-    // http://flashphoner.com/docs/wcs4/wcs_docs/html/en/wcs-developer-guide/quick_start_recording_streams.htm
-    var link = window.location.protocol + "//" + window.location.host + '/client/records/' + name;
-    $("#link").attr("href", link);
-    $("#recVideo").attr("src", link).attr("controls", true);
-    $("#downloadDiv").show();
-    _fileName = null;
+    if (name) {
+        // Set correct path for records. Stream records are saved to WCS_HOME/records directory.
+        // http://flashphoner.com/docs/wcs4/wcs_docs/html/en/wcs-developer-guide/quick_start_recording_streams.htm
+        var link = window.location.protocol + "//" + window.location.host + '/client/records/' + name;
+        $("#link").attr("href", link);
+        $("#recVideo").attr("src", link).attr("controls", true);
+        $("#downloadDiv").show();
+    }
+}
+
+function validateForm() {
+    var valid = true;
+    $(':text').each(function(){
+        if (!$(this).val()) {
+            highlightInput($(this));
+            valid = false;
+        } else {
+            removeHighlight($(this));
+        }
+    });
+    return valid;
+
+    function highlightInput(input) {
+        input.closest('.input-group').addClass("has-error");
+    }
+    function removeHighlight(input) {
+        input.closest('.input-group').removeClass("has-error");
+    }
 }
