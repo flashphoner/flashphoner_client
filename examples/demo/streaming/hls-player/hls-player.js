09b94194f82e600afc7345bbd2447584314d1148
Flashphoner <support@flashphoner.com>
2017-05-10 16:22:31 +0700

    User base64 for hls

--- examples/demo/streaming/hls-player/hls-player.js
+++ examples/demo/streaming/hls-player/hls-player.js
@@ -3,6 +3,7 @@ function initPage() {
     var applyFn = function () {
         var streamName = $("#playStream").val();
         var $remoteVideo = $("#remoteVideo");
+        streamName = btoa(streamName);
         $remoteVideo.attr("src", $("#urlServer").val() + "/" + streamName + "/" + streamName + ".m3u8");
         $remoteVideo.load();
     };
