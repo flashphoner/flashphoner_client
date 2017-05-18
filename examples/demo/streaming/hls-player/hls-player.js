7a58593d38f213eee35cf3bbf5c2c7f154a1931c
Flashphoner <support@flashphoner.com>
2017-05-19 00:10:23 +0700

    HLS video-js

--- examples/demo/streaming/hls-player/hls-player.js
+++ examples/demo/streaming/hls-player/hls-player.js
@@ -1,11 +1,14 @@
 function initPage() {
     $("#urlServer").val(getHLSUrl());
+    var player = videojs('remoteVideo');
     var applyFn = function () {
         var streamName = $("#playStream").val();
-        var $remoteVideo = $("#remoteVideo");
         streamName = btoa(streamName);
-        $remoteVideo.attr("src", $("#urlServer").val() + "/" + streamName + "/" + streamName + ".m3u8");
-        $remoteVideo.load();
+        player.src({
+            src: $("#urlServer").val() + "/" + streamName + "/" + streamName + ".m3u8",
+            type: "application/vnd.apple.mpegurl"
+        });
+        player.play();
     };
     $("#applyBtn").prop('disabled', false).click(applyFn);
 
