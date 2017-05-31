61a07b7bc176182fcbe485ac2a34fee5b43240c3
Flashphoner <support@flashphoner.com>
2017-05-31 09:52:09 +0400

    [FIX] Player minor changes

--- examples/demo/streaming/player/player.js
+++ examples/demo/streaming/player/player.js
@@ -131,6 +131,9 @@ function playStream(session) {
     }
     stream = session.createStream(options).on(STREAM_STATUS.PLAYING, function(stream) {
         $("#preloader").show();
+        // For WSPlayer
+        if (stream.getInfo() == "FIRST_FRAME_RENDERED")
+            $("#preloader").hide();
         document.getElementById(stream.id()).addEventListener('resize', function(event){
             $("#preloader").hide();
             var streamResolution = stream.videoResolution();
