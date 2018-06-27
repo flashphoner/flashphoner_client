bafcf45e548845347538592d63e16d8b55aefbae
Igor <igor@flashphoner.com>
2018-06-27 09:18:13 +0400

    remove speaker list for mobile browsers

--- examples/demo/streaming/media_devices_manager/manager.js
+++ examples/demo/streaming/media_devices_manager/manager.js
@@ -238,6 +238,9 @@ function init_page() {
             previewStream.setAudioOutputId($(this).val());
         }
     });
+    if (!Browser.isChrome()) {
+        $('#audioOutput').remove();
+    }
 }
 
 function onStarted(publishStream, previewStream) {
