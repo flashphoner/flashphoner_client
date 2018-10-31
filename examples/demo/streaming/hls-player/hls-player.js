a153d5e4a2b4a1a0781f74a66fc633a78af6db3f
Flashphoner <support@flashphoner.com>
2018-10-31 11:00:03 +0700

    #WCS-1284 - HLS authentication via REST API и hooks

--- examples/demo/streaming/hls-player/hls-player.js
+++ examples/demo/streaming/hls-player/hls-player.js
@@ -4,8 +4,13 @@ function initPage() {
     var applyFn = function () {
         var streamName = $("#playStream").val();
         streamName = encodeURIComponent(streamName);
+        var src = $("#urlServer").val() + "/" + streamName + "/" + streamName + ".m3u8";
+        var token = $("#token").val();
+        if (token.length > 0) {
+            src += "?token=" +token;
+        }
         player.src({
-            src: $("#urlServer").val() + "/" + streamName + "/" + streamName + ".m3u8",
+            src: src,
             type: "application/vnd.apple.mpegurl"
         });
         player.play();
