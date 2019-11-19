f494d1936d3354a8133d7cf9bf8de0376b390396
Flashphoner <support@flashphoner.com>
2019-11-19 19:00:02 +0700

    Merge branch 'wcs_api-2.0' of gitlab.flashphoner.com:flashphoner/WEB-SDK into wcs_api-2.0 added MCU client example #WCS-2358:

---
+++ examples/demo/streaming/mcu_client/mcu_client.js
@@ -0,0 +1,241 @@
+var SESSION_STATUS = Flashphoner.constants.SESSION_STATUS;
+var STREAM_STATUS = Flashphoner.constants.STREAM_STATUS;
+var PRELOADER_URL = "../../dependencies/media/preloader.mp4";
+var remoteVideo;
+var conferenceStream;
+var publishStream;
+var currentVolumeValue = 50;
+var mockLocalDisplay;
+
+function init_page() {
+
+    //init api
+    try {
+        Flashphoner.init({
+            flashMediaProviderSwfLocation: '../../../../media-provider.swf'
+        });
+    } catch (e) {
+        $("#notifyFlash").text("Your browser doesn't support Flash or WebRTC technology needed for this example");
+        return;
+    }
+
+    //video display
+    remoteVideo = document.getElementById("remoteVideo");
+    mockLocalDisplay = document.createElement("div");
+
+    $("#url").val(setURL());
+    $("#volumeControl").slider({
+        range: "min",
+        min: 0,
+        max: 100,
+        value: currentVolumeValue,
+        step: 10,
+        animate: true,
+        slide: function (event, ui) {
+            currentVolumeValue = ui.value;
+            conferenceStream.setVolume(currentVolumeValue);
+        }
+    }).slider("disable");
+    if (Flashphoner.getMediaProviders()[0] == "Flash") {
+        $("#fullScreen").hide();
+    }
+    onStopped();
+}
+
+function onStarted() {
+    $("#joinBtn").text("Leave").off('click').click(function () {
+        $(this).prop('disabled', true);
+        stopStreams();
+    }).prop('disabled', false);
+    $("#fullScreenBtn").off('click').click(function () {
+        conferenceStream.fullScreen();
+    }).prop('disabled', false);
+    $("#hasAudio").prop('disabled', true);
+    $("#volumeControl").slider("enable");
+    conferenceStream.setVolume(currentVolumeValue);
+}
+
+function onStopped() {
+    $("#joinBtn").text("Join").off('click').click(playBtnClick).prop('disabled', false);
+    $('#url').prop('disabled', false);
+    $("#room").prop('disabled', false);
+    $("#login").prop('disabled', false);
+    $("#volumeControl").slider("disable");
+    $("#fullScreenBtn").prop('disabled', true);
+    $("#hasAudio").prop('disabled', false);
+    $("#preloader").hide();
+    stopStreams();
+}
+
+function stopStreams() {
+    conferenceStream.stop();
+    publishStream.stop();
+}
+
+function playBtnClick() {
+    if (validateForm()) {
+        $(this).prop('disabled', true);
+        $('#url').prop('disabled', true);
+        $("#room").prop('disabled', true);
+        $("#login").prop('disabled', true);
+        if (Browser.isSafariWebRTC() || Flashphoner.getMediaProviders()[0] === "MSE") {
+            Flashphoner.playFirstVideo(remoteVideo, false, PRELOADER_URL).then(function () {
+                start();
+            });
+            return;
+        }
+        start();
+    }
+}
+
+function start() {
+    var url = $('#url').val();
+    //check if we already have session
+    if (Flashphoner.getSessions().length > 0) {
+        var session = Flashphoner.getSessions()[0];
+        if (session.getServerUrl() == url) {
+            startStreaming(session);
+            return;
+        } else {
+            //remove session DISCONNECTED and FAILED callbacks
+            session.on(SESSION_STATUS.DISCONNECTED, function () {
+            });
+            session.on(SESSION_STATUS.FAILED, function () {
+            });
+            session.disconnect();
+        }
+    }
+    //create session
+    console.log("Create new session with url " + url);
+    Flashphoner.createSession({urlServer: url}).on(SESSION_STATUS.ESTABLISHED, function (session) {
+        setStatus(session.status());
+        //session connected, start playback
+        startStreaming(session);
+    }).on(SESSION_STATUS.DISCONNECTED, function () {
+        setStatus(SESSION_STATUS.DISCONNECTED);
+        onStopped();
+    }).on(SESSION_STATUS.FAILED, function () {
+        setStatus(SESSION_STATUS.FAILED);
+        onStopped();
+    });
+
+}
+
+function startStreaming(session) {
+    var roomName = $('#room').val();
+    var login = $('#login').val();
+    var streamName = login + "#" + roomName;
+    publishStream = session.createStream({
+        name: streamName,
+        display: mockLocalDisplay,
+        receiveVideo: false,
+        receiveAudio: false,
+        constraints: getConstraints()
+    }).on(STREAM_STATUS.PUBLISHING, function (publishStream) {
+        //play preview
+        playStream(session);
+    }).on(STREAM_STATUS.UNPUBLISHED, function () {
+        onStopped();
+    }).on(STREAM_STATUS.FAILED, function (stream) {
+        setStatus(STREAM_STATUS.FAILED, "This login is already in use. Please change login");
+        onStopped();
+    });
+    publishStream.publish();
+}
+
+
+function playStream(session) {
+    var roomName = $('#room').val();
+    var login = $('#login').val();
+    var streamName = roomName + "-" + login + roomName;
+
+    conferenceStream = session.createStream({
+        name: streamName,
+        display: remoteVideo,
+        constraints: getConstraints()
+    }).on(STREAM_STATUS.PENDING, function (stream) {
+        $("#preloader").show();
+        var video = document.getElementById(stream.id());
+        if (!video.hasListeners) {
+            video.hasListeners = true;
+            //don't resize html5 video
+            if (video.nodeName.toLowerCase() !== "video") {
+                video.addEventListener('resize', function (event) {
+                    var streamResolution = stream.videoResolution();
+                    console.log("Stream resolution " + streamResolution);
+                    if (Object.keys(streamResolution).length === 0) {
+                        resizeVideo(event.target);
+                    } else {
+                        // Change aspect ratio to prevent video stretching
+                        var ratio = streamResolution.width / streamResolution.height;
+                        var newHeight = Math.floor(options.playWidth / ratio);
+                        resizeVideo(event.target, options.playWidth, newHeight);
+                    }
+                });
+            }
+        }
+    }).on(STREAM_STATUS.PLAYING, function (stream) {
+        $("#preloader").hide();
+        setStatus(stream.status());
+        onStarted();
+    }).on(STREAM_STATUS.STOPPED, function () {
+        $("#preloader").hide();
+        setStatus(STREAM_STATUS.STOPPED);
+        onStopped();
+    }).on(STREAM_STATUS.FAILED, function (stream) {
+        $("#preloader").hide();
+        setStatus(STREAM_STATUS.FAILED, stream.getInfo());
+        onStopped();
+    }).on(STREAM_STATUS.NOT_ENOUGH_BANDWIDTH, function (stream) {
+        console.log("Not enough bandwidth, consider using lower video resolution or bitrate. Bandwidth " + (Math.round(stream.getNetworkBandwidth() / 1000)) + " bitrate " + (Math.round(stream.getRemoteBitrate() / 1000)));
+    });
+    conferenceStream.play();
+}
+
+function getConstraints() {
+    var constraints = {
+        audio: $("#hasAudio").is(':checked'),
+        video: true
+    };
+    return constraints;
+}
+
+//show connection or remote stream status
+function setStatus(status, info) {
+    var statusField = $("#status");
+    var infoField = $("#info");
+    statusField.text(status).removeClass();
+    if (status == "PLAYING") {
+        statusField.attr("class", "text-success");
+        infoField.text("");
+    } else if (status == "DISCONNECTED" || status == "ESTABLISHED" || status == "STOPPED") {
+        statusField.attr("class", "text-muted");
+        infoField.text("");
+    } else if (status == "FAILED") {
+        statusField.attr("class", "text-danger");
+        if (info) {
+            infoField.text(info).attr("class", "text-muted");
+        }
+    }
+}
+
+function validateForm() {
+    var valid = true;
+    $('#form :text').each(function () {
+        if (!$(this).val()) {
+            highlightInput($(this));
+            valid = false;
+        } else {
+            removeHighlight($(this));
+        }
+    });
+    return valid;
+
+    function highlightInput(input) {
+        input.closest('.form-group').addClass("has-error");
+    }
+
+    function removeHighlight(input) {
+        input.closest('.form-group').removeClass("has-error");
+    }
+}
