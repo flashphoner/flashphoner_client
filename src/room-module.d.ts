8e85db6c1c6a41529a387e3552932e57ee6a33e9
SOPREX\marko.nastic <marko.nastic@soprex.com>
2023-01-30 12:33:48 +0100

    Implemented missing typescript clases and properties for roomApi implementation

--- src/room-module.d.ts
+++ src/room-module.d.ts
@@ -9,6 +9,8 @@
  * @param {Object} options session options
  * @param {String} options.urlServer Server address in form of [ws,wss]://host.domain:port
  * @param {String} options.username Username to login with
+ * @param {String} options.token JWT token
+ * @param {String} options.appKey Application key
  * @returns {roomApi.Session}
  * @memberof roomApi
  * @method connect
@@ -17,7 +19,34 @@ import Flashphoner = require("./flashphoner-core");
 declare function appSession(options: {
     urlServer: string;
     username: string;
-}): Flashphoner.Session;
+    token?: string;
+    appKey?: string;
+}): RoomSession;
+export declare class RoomSession extends Flashphoner.Session {
+    getRooms: () => Room[];
+    join: ({name, record}: {name: string, record: boolean}) => Room;
+    username: () => string;
+}
+export declare class Room {
+    name: () => string;
+    leave: () => Promise<any>;
+    publish: (options: Flashphoner.CreateStreamOptions) => any;
+    getParticipants: () => Participant[];
+    on: (event: string, callback: any) => Room;
+}
+export declare class Participant {
+    getStreams: () => ParticipantStream[];
+    name: () => string;
+    sendMessage: (text: any, error: any) => void; //msg in chat (text - msg to send; error- err callback)
+    streams: ParticipantStreams;
+}
+export type ParticipantStreams = { [key: string]: ParticipantStream };
+export declare class ParticipantStream {
+    play: (display: any, options?: {}) => Flashphoner.Stream;
+    stop: () => void;
+    id: () => string;
+    streamName: () => string
+}
 export namespace events {
     const STATE: string;
     const JOINED: string;
